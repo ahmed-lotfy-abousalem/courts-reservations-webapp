58b72150b1c6f2e84aa020c352f8860fd74ca588
ahmed-lotfy-abousalem <ahmedlotfy122@yahoo.com>
2023-05-18 22:43:37 +0300

    contactUs Back-End

--- Contact_Us/views.py
+++ Contact_Us/views.py
@@ -3,5 +3,12 @@ from django.http import HttpResponse
 
 def Contact_Us(request):
     
-    return render(request,'Contact_Us/contactus.html')
+    if request.method =="POST":
+        fullname = request.POST['fullname']
+        email = request.POST['email']
+        message = request.POST['message']
+        return render (request,'Contact_Us/contactus.html', {'fullname' : fullname } )
+        
+    else:
+      return render(request,'Contact_Us/contactus.html')
 # Create your views here.
