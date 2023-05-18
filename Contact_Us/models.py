94bd7fd36a0b5b984249d6bdadaf306501faeaa5
ahmed-lotfy-abousalem <ahmedlotfy122@yahoo.com>
2023-05-18 23:34:17 +0300

    BACK-END ContactING Us

--- Contact_Us/models.py
+++ Contact_Us/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+        fullname = models.CharField(max_length=200)
+        email = models.EmailField()
+        message = models.TextField()
+        def __str__(self):
+                return self.fullname
--- Contact_Us/views.py
+++ Contact_Us/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import contact
+from django.contrib import messages
 
 def Contact_Us(request):
     
     if request.method =="POST":
+        cntact = contact()
         fullname = request.POST['fullname']
         email = request.POST['email']
         message = request.POST['message']
-        return render (request,'Contact_Us/contactus.html', {'fullname' : fullname } )
+        
+        cntact.fullname = fullname
+        cntact.email = email
+        cntact.message = message
+        
+        cntact.save()
+        messages.success(request,"Your Message Has Been sent")
+        return render (request,'Contact_Us/contactus.html' )
         
     else:
       return render(request,'Contact_Us/contactus.html')
