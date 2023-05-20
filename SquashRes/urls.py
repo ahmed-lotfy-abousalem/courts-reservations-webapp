32ac159124e29d7d87e522c71a031b917235b6a4
ahmed-lotfy-abousalem <ahmedlotfy122@yahoo.com>
2023-05-20 03:21:38 +0300

    all done

--- SquashRes/urls.py
+++ SquashRes/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.SquashRes, name="SquashRes"),
+    path('save-date/', views.save_date, name='save_date'),
+    path('success/<str:selected_date>/', views.success, name='success'),
 ]
